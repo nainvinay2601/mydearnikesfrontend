b8bc13d812f645cd5f87854ce7e8e21108e5d59a
Vinay Nain <vinaynain2601@gmail.com>
2025-08-24 22:02:36 +0530

    Component seperation

---
+++ src/components/Navbar/SearchBar/SearchBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SearchBar = () => {
+  return (
+   <div className="px-[8px] py-3 border-b-[0.5px] border-[#aeadad]">
+                <input type="text" placeholder="Type Something ....." className="w-full" />
+              </div>
+  )
+}
+
+export default SearchBar
--- src/components/Navbar/SearchComponent/SearchComp.tsx
+++ src/components/Navbar/SearchComponent/SearchComp.tsx
@@ -1,62 +1,14 @@
-import { Button } from '@/components/ui/button'
-import Image from 'next/image'
-import React from 'react'
+import React from "react";
+import SearchBar from "../SearchBar/SearchBar";
+import NavCarousel from "../NavImageCarousel/NavCarousel";
 
 const SearchComponent = () => {
   return (
-     <div>
-              <div className="px-[8px] py-3 border-b-[0.5px] border-[#aeadad]">
-                <input type="text" placeholder="Type Something ....." className="w-full" />
-              </div>
-              <div className="px-[8px] py-3 flex item-center gap-2 overflow-x-auto scrollbar-hide border-b-[0.5px] border-[#aeadad]">
-                <div className="border-[0.5px] border-[#aeadad] rounded-xl bg-[#f2f2f2] w-[240px] h-[280px] flex flex-col items-center justify-center ">
-                  <div className="w-[220px] h-[220px] relative">
-                    <Image
-                      src={"/images/MDNPink.webp"}
-                      alt="test Image"
-                      fill
-                      objectFit="cover"
-                      className="  "
-                    />
-                  </div>
+    <>
+      <SearchBar />
+      <NavCarousel />
+    </>
+  );
+};
 
-                  <Button variant={"outline"} className="rounded-full">
-                    Baby Tees
-                  </Button>
-                </div>
-                <div className="border-[0.5px] border-[#aeadad] rounded-xl bg-[#f2f2f2] w-[240px] h-[280px] flex flex-col items-center justify-center ">
-                  <div className="w-[220px] h-[220px] relative">
-                    <Image
-                      src={"/images/MDNPink.webp"}
-                      alt="test Image"
-                      fill
-                      objectFit="cover"
-                      className="  "
-                    />
-                  </div>
-
-                  <Button variant={"outline"} className="rounded-full">
-                    Baby Tees
-                  </Button>
-                </div>
-                <div className="border-[0.5px] border-[#aeadad] rounded-xl bg-[#f2f2f2] w-[240px] h-[280px] flex flex-col items-center justify-center ">
-                  <div className="w-[220px] h-[220px] relative">
-                    <Image
-                      src={"/images/MDNPink.webp"}
-                      alt="test Image"
-                      fill
-                      objectFit="cover"
-                      className="  "
-                    />
-                  </div>
-
-                  <Button variant={"outline"} className="rounded-full">
-                    Baby Tees
-                  </Button>
-                </div>
-              </div>
-            </div>
-  )
-}
-
-export default SearchComponent
+export default SearchComponent;
